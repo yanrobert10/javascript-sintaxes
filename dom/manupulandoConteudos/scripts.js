dfdc0c5a4d2254bbfe911977eacbd1a1d31db676
yanrobert10 <yan39robert@gmail.com>
2023-06-01 11:53:15 -0300

    add more examples

--- dom/manupulandoConteudos/scripts.js
+++ dom/manupulandoConteudos/scripts.js
@@ -37,3 +37,40 @@ console.log(headerID.getAttribute("class")); // verificando, pegar o atributo cl
 
 header.removeAttribute("id"); // remover atributo
 header.removeAttribute("class"); 
+
+// manipulando estilos e classes
+
+const element1 = document.querySelector("body ");
+element1.classList.add("active", "red"); // adicionar
+console.log(element1.classList);
+element1.classList.remove("active"); // remover
+element1.classList.toggle("active"); // interruptor se eu add ele tira
+console.log(element1.classList);
+
+
+// Navegando pelos elementos 
+
+console.log(header.parentElement);// Retornao o elemento pai
+console.log(header.parentNode);// Retornao o elemento pai
+
+console.log(element1.childNodes); // Retornao todos elementos filhos
+console.log(element1.firstElementChild); // Retornao o primeiro elemento filho
+console.log(element1.lastElementChild); // Retornao o ultimo elemento filho
+console.log(element1.nextElementSibling); // Retornao o proximo elemento filho
+console.log(element1.nextSibling); // Retornao a proxima linha do elemento filho
+console.log(element1.previousElementSibling); // Retornao o filho anterior
+
+
+// Criando e adicionando elementos
+
+//criar elemento
+const div = document.createElement("div");
+div.innerText = "Olá Devs!!";
+
+// append prenped
+element1.append(div); // adicionar elemento depois
+
+// insertBefore elemento
+const script = document.createElement("script");
+element1.insertBefore(div, script); // inserir elemento antes da tag script
+element1.insertBefore(div, header.nextElementSibling); // inserir elemento depois da tag header
